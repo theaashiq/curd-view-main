a6e8890c09139e5c02d95087c3954eea8d9146a7
theaashiq <mdaashiqin2000@gmail.com>
2024-09-30 19:35:08 +0530

    updates

--- src/pages/customers/customersList.jsx
+++ src/pages/customers/customersList.jsx
@@ -18,6 +18,12 @@ const CustomersList = () => {
                 Date of Birth <span>17.07.1994</span>
             </div>
             <div>
+                Age <span>28</span>
+            </div>
+            <div>
+                Gender <span>Male</span>
+            </div>
+            <div>
                 Phone Number <span>+91 9448343941</span>
             </div>
             <div>
