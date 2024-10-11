852a20cb629a0107f898ed573588d9d8d276772f
theaashiq <mdaashiqin2000@gmail.com>
2024-10-11 21:29:16 +0530

    updates

--- src/pages/customers/CustomerDelete.jsx
+++ src/pages/customers/CustomerDelete.jsx
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import CloseIcon from '@mui/icons-material/Close';
 import './customerDelete.css'
 import { deleteData } from '../../services/operationServices';
 import { MainContext } from '../../context/mainContext.jsx';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const CustomerDelete = (props) => {
 
@@ -10,11 +11,18 @@ const { setDeleteToggle, id, firstName, lastName } = props
 
 const { getData } = useContext(MainContext)
 
+const [ deleteNotify, setDeleteNotify ] = useState(false)
+const [ loading, setLoading ] = useState(false)
+
 const handleDelete = async () => {
+    setLoading(true)
     let postData = { id : id }
-    await deleteData(postData)
-    setDeleteToggle(false)
-    getData('name')
+    const response = await deleteData(postData)
+    console.log(response, 'RESPONSE')
+    if(response.message === "Deleted Sucessfully") {
+        setDeleteNotify(true)
+        setLoading(false)
+    }
 }
 
 
@@ -23,22 +31,34 @@ const handleDelete = async () => {
 
     <React.Fragment>
         <div className='customerDelete-container'>
-            <div className='customerDelete-Block' style={{display: 'flex', flexDirection:'column', justifyContent:'space-between'}}>
-                <div className='customerDelete-header'>
-                    Delete Customer 
-                    <CloseIcon onClick={() => setDeleteToggle(false)}/>
-                </div>
-                <div className='customerDelete-details'>
-                    <p>Do you want to delete <span style={{textTransform:'capitalize'}}>{firstName} {lastName}</span> information?</p>
-                </div>
-                <div className='customerDelete-btns'>
-                    <button onClick={() => setDeleteToggle(false)}>
-                        No
-                    </button>
-                    <button onClick={() => handleDelete()}>
-                        Yes
-                    </button>
-                </div>
+            <div className='customerDelete-Block' style={{minHeight:'100px', minWidth:'260px'}}>
+               {!deleteNotify 
+                ?   <div style={{display: 'flex', flexDirection:'column', justifyContent:'space-between',}}>
+                        {loading && <div className='createCustomer-loading'>
+                            <img src='loading.gif' />
+                        </div>}
+                        <div className='customerDelete-header'>
+                            Delete Customer 
+                            <CloseIcon onClick={() => setDeleteToggle(false)}/>
+                        </div>
+                        <div className='customerDelete-details'>
+                            <p>Do you want to delete <span style={{textTransform:'capitalize'}}>{firstName} {lastName}</span> information?</p>
+                        </div>
+                        <div className='customerDelete-btns'>
+                            <button onClick={() => setDeleteToggle(false)}>
+                                No
+                            </button>
+                            <button onClick={() => handleDelete()}>
+                                Yes
+                            </button>
+                        </div>
+                    </div>
+                :   <div className='customerDelete-nofify'>
+                        <div>Deleted Successfully <DeleteIcon /> </div>
+                        <button onClick={() => {setDeleteToggle(false), getData('date')}}>
+                            Done
+                        </button> 
+                    </div>}
             </div>
         </div>
     </React.Fragment>
